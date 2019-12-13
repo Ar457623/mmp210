dc58cf0ade5ff96307281037d9af7119884f0678
Ar457623 <44677633+Ar457623@users.noreply.github.com>
2019-12-12 22:31:50 -0500

    update

--- P5sketchsound/sketch.js
+++ P5sketchsound/sketch.js
@@ -56,15 +56,56 @@ function draw() {
 	}
     
     
+        
+fill("Black");
+    noStroke();
+    ellipse(252, 120, 72, 72);
+    
+    fill(255);
+  ellipse(550, 290, 72, 72);
+    
+    
+    fill("white");
+  ellipse(50, 90, 72, 72);
+    
+    fill("Black");
+  ellipse(50, 350, 72, 72);
+    
+    
+   fill(255);
+  ellipse(250, 330, 72, 72);
+    
+    fill("Black");
+  ellipse(530, 50, 72, 72);
+    
+   fill("White");
+  rect(130, 20, 63, 63);
+    
+    fill("White");
+  rect(380, 60, 33, 93);
+    
+    fill("white");
+  rect(125, 300, 43, 95);
+    
+    fill("Black");
+  rect(360, 310, 80, 30);
+    
+    
+    triangle(30, 75, 58, 20, 86, 75);
+    
+    fill("White");
+    triangle(100, 100, 100, 200, 10, 290);
+    
+      
+    fill("Black");
+    triangle(500, 200, 400, 200, 550, 290);
+    
+	
     fill("black");
      textSize(40);
      textAlign(CENTER, CENTER);
     text(sensorValue, width/2, height/2);
-    
-    
-    
-   
-    
+       
 	
 }
 
